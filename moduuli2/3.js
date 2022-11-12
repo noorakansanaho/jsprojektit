6e43046c6075c9ac24079e73f8bb75ba2c251faa
noorakansanaho <noora.kansanaho@metropolia.fi>
2022-11-12 15:19:15 +0200

    .

---
+++ moduuli2/3.js
@@ -0,0 +1,10 @@
+'use strict';
+const koirat = [];
+for(let i = 1; i <= 6; i++){
+  koirat.push(prompt(`Syötä ${i}. koiran nimi`));
+}
+koirat.sort();
+koirat.reverse();
+for(let koira of koirat){
+  document.getElementById('target').innerHTML += `<li>${koira}</li>`;
+}
