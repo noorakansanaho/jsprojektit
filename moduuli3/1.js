a5420eb6db3926d2a33c9f6f597387bd379a0e3c
noorakansanaho <noora.kansanaho@metropolia.fi>
2022-11-27 14:34:14 +0200

    .

--- moduuli3/1.js
+++ moduuli3/1.js
@@ -4,3 +4,6 @@ document.getElementById('target').innerHTML += `
 <li>First item</li>
 <li>Second item</li>
 <li>Third item</li>`
+
+const target = document.getElementById('target');
+target.classList.add("my-list");
